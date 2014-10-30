1a66809e3a3b1e469295a6f8360f7a18466d5f01
JJ <joao.jeronimo@ist.utl.pt>
2014-10-30 19:41:20 +0000

    Alternating Blinkings......

--- arduino/smart-irrigation.cpp
+++ arduino/smart-irrigation.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
-void setup() {
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 7;
 
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
+int delayamount = 1000;
+
+// the loop routine runs over and over again forever:
 void loop() {
-  
+  digitalWrite(led, HIGH);
+  digitalWrite(13, LOW);
+  delay(delayamount);
+  digitalWrite(led, LOW);
+  digitalWrite(13, HIGH);
+  delay(delayamount);
 }
