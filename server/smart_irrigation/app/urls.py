df8615e901d1fa88c49648bfa3f7a73ebdb4e081
Samuel <samuelfcmc@gmail.com>
2014-11-17 13:27:29 +0000

    Added user authentication

--- server/smart_irrigation/app/urls.py
+++ server/smart_irrigation/app/urls.py
@@ -7,5 +7,7 @@ from app import views
 
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
+		url(r'^logout/', views.user_logout, name='logout'),
+		url(r'^login/', views.user_login, name='login'),
 		url(r'^api/status', views.status, name='status'),
 )
--- server/smart_irrigation/app/views.py
+++ server/smart_irrigation/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import logout, login, authenticate
 import json
 
 # Return json
@@ -8,7 +9,30 @@ def getJson(response):
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	if(request.user.is_authenticated()):
+		view = 'index.html'
+	else:
+		view = 'login.html'
+
+	return render(request, view)
+
+def user_logout(request):
+	logout(request)
+	return index(request)
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(username=username, password=password)
+		print(user)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+	return index(request)
+
 
 def status(request):
 	response = {'name': 'Test',
